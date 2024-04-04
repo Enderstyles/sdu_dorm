ad36810399f69abfa3708780a846478df1e34e35
Al-Aziz Askhat <alaziz.200223@gmail.com>
2024-04-04 17:28:44 +0500

    settings: port change for hosting

--- eazzy/settings.py
+++ eazzy/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.0.102:8080",
     "http://alazizaskhat.pythonanywhere.com",
     "https://www.sdudorm.kz",
-    "http://77.243.80.198",
+    "http://77.243.80.198:8000",
 ]
 
 MIDDLEWARE = [
