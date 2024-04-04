feeec375bc198833c0aa3224af1238898c6c68d3
Al-Aziz Askhat <alaziz.200223@gmail.com>
2024-04-04 17:51:45 +0500

    settings: adding cors headers

--- eazzy/settings.py
+++ eazzy/settings.py
@@ -88,6 +88,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://alazizaskhat.pythonanywhere.com",
     "https://www.sdudorm.kz",
     "http://77.243.80.198:8000",
+    "http://77.243.80.198",
+    "http://77.243.80.198:8080"
 ]
 
 MIDDLEWARE = [
