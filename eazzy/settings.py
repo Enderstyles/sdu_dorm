b9184b50d6958c622c372ab343907a4970a05fd5
Al-Aziz Askhat <alaziz.200223@gmail.com>
2024-04-14 18:52:40 +0500

    hot fix

--- eazzy/settings.py
+++ eazzy/settings.py
@@ -13,6 +13,8 @@ from datetime import timedelta
 from pathlib import Path
 import os
 
+from django.template.context_processors import static
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -97,7 +99,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
